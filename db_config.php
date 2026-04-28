ce893fafa8eb312ae1fa9fa13e847fe33cc3944a
denis22078533-coder <denis22078533@gmail.com>
2026-04-28 23:09:34 +0300

    Create db_config.php

---
+++ db_config.php
@@ -0,0 +1,7 @@
+```php
+<?php
+$servername = "localhost"; // Хост
+$username = "your_username"; // Логин
+$password = "your_password"; // Пароль
+$dbname = "coffee_shop"; // Название базы данных
+?>
